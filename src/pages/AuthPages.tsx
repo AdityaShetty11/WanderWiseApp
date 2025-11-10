2c1e9803e5b9f2b4d9832433ede2eef5d87b6487
Aditya Shetty <32506113+AdityaShetty11@users.noreply.github.com>
2025-11-10 10:00:00 +0000

    Create initial authentication pages (Signup and Login)

---
+++ src/pages/AuthPages.tsx
@@ -0,0 +1,10 @@
+import { SignupComponent } from "../components/SignupComponent";
+import { LoginComponent } from "../components/LoginComponent";
+
+export function SignupPage() {
+  return <SignupComponent />;
+}
+
+export function LoginPage() {
+  return <LoginComponent />;
+}
